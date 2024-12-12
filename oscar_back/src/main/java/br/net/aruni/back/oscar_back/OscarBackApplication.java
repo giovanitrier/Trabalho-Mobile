e6735663b2b4c1290a650116208f9a6f4162296d
aruniamstel <bioaruni@gmail.com>
2024-12-12 17:34:11 -0300

    Projeto Back-end criado.

---
+++ oscar_back/src/main/java/br/net/aruni/back/oscar_back/OscarBackApplication.java
@@ -0,0 +1,13 @@
+package br.net.aruni.back.oscar_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OscarBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OscarBackApplication.class, args);
+	}
+
+}
